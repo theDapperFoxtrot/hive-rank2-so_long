4373bda6115d00e35591991b0f5a706d164f4581
theDapperFoxtrot <thedapperfoxtrot.bizinq@gmail.com>
2024-11-29 00:01:39 +0200

    starting

--- main_so_long.c
+++ main_so_long.c
@@ -1,4 +1,6 @@
-int main ()
+#include "so_long.h"
+
+int32_t	main(void)
 {
     return (0);
 }
---
+++ so_long.h
@@ -0,0 +1,7 @@
+#ifndef SO_LONG_H
+# define SO_LONG_H
+
+# include "libft/libft.h"
+# include "lib/MLX42/include/MLX42/MLX42.h"
+
+#endif
